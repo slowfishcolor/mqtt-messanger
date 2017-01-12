4dbec03f04668040dfeefd50a6fcc33efd3ce4f1
Prophet Xu <slowfishcolor@gmail.com>
2017-01-12 17:00:48 +0800

    Setup ActiveMQ, add stomp over websocket for 'text' page.

---
+++ src/main/webapp/resources/js/text.js
@@ -0,0 +1,83 @@
+/**
+ * Created by Prophet on 2017/1/10.
+ */
+$(document).ready(function () {
+
+    var client,receiveDestination, sendDestination;
+
+    /**
+     * connect and subscribe
+     */
+    $('#subscribe-button').click(function () {
+        var url = "ws://localhost:61614/stomp";
+        var login = "";
+        var passcode = "";
+        receiveDestination = "/topic/" + $('#topic').val();
+
+        sendDestination = "/topic/messanger.topic.device"
+
+        // Stomp client
+        client = Stomp.client(url);
+
+        // log debug message
+        client.debug = function (str) {
+            console.log(str);
+        };
+
+        // the client is notified when it is connected to the server.
+        // on connect callback
+        var onconnect = function (frame) {
+            client.debug("connect to Stomp");
+
+            $('#topic').attr("disabled", true);
+            $('#subscribe-button').attr("disabled", true);
+            $('#unsubscribe-button').attr("disabled", false);
+            $('#send-button').attr("disabled", false);
+
+            // subscribe receive topic
+            client.subscribe(receiveDestination, function (message) {
+                var d = document.createElement("div");
+                d.setAttribute("class", "list-group-item");
+                var t = document.createTextNode(message.body);
+                d.appendChild(t);
+
+                $(d).hide().prependTo($('#receive-list')).slideDown();
+            })
+        };
+
+        // connect to broker
+        client.connect(login, passcode, onconnect);
+    });
+
+    /**
+     * disconnect
+     */
+    $('#unsubscribe-button').click(function () {
+        client.disconnect(function () {
+            // disconnect call back
+            $('#topic').attr("disabled", false);
+            $('#subscribe-button').attr("disabled", false);
+            $("#unsubscribe-button").attr("disabled", true)
+            $('#send-button').attr("disabled", true);
+
+        });
+    });
+
+    /**
+     * send message
+     */
+    $('#send-button').click(function () {
+        var text = $('#send-message').val();
+        if (text) {
+            // send message
+            client.send(sendDestination, {foo: 1}, text);
+            // gui change
+            var d = document.createElement("div");
+            d.setAttribute("class", "list-group-item");
+            var t = document.createTextNode($('#send-message').val());
+            d.appendChild(t);
+            $(d).hide().prependTo($('#send-list')).slideDown();
+            $('#send-message').val("");
+        }
+    })
+});
